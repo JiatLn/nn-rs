67493875b43ba13ac2691bc04b754d79a8c31681
cx33 <lne128gene980@163.com>
2023-09-04 12:14:59 +0800

    refactor: conv layer

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ download link: http://yann.lecun.com/exdb/mnist
 ### CNN
 
 https://victorzhou.com/blog/intro-to-cnns-part-1
+https://victorzhou.com/blog/intro-to-cnns-part-2
--- src/cnn/conv.rs
+++ src/cnn/conv.rs
@@ -26,25 +26,15 @@ impl ConvLayer {
 
     pub fn forward(&mut self, input: &Matrix<f64>) -> Vec<Matrix<f64>> {
         let (h, w) = input.shape();
-        if h < 2 || w < 2 {
-            panic!("The input shape must be greater then 2x2");
+        if h < self.filter_size || w < self.filter_size {
+            panic!("The input shape must be greater then filter size");
         }
-        self.last_input = input.clone();
-        let mut output = (0..self.filter_num)
-            .map(|_| Matrix::new_zero(h - 2, w - 2))
-            .collect::<Vec<_>>();
 
-        for i in 0..h - 2 {
-            for j in 0..w - 2 {
-                let slice_martix = input.slice(i, j, self.filter_size);
-                for k in 0..self.filter_num {
-                    let filter = &self.filters[k];
-                    let sum = slice_martix.multiple_martix_sum(filter);
-                    output[k].set(i, j, sum);
-                }
-            }
-        }
-        output
+        self.last_input = input.clone();
+
+        (0..self.filter_num)
+            .map(|i| convolve(input, &self.filters[i]))
+            .collect()
     }
 
     pub fn backprop(&mut self, d_l_d_out: &Vec<Matrix<f64>>, lr: f64) -> () {
@@ -54,19 +44,37 @@ impl ConvLayer {
 
         let (h, w) = self.last_input.shape();
 
-        for i in 0..h - 2 {
-            for j in 0..w - 2 {
-                let slice_martix = self.last_input.slice(i, j, self.filter_size);
-                for k in 0..self.filter_num {
-                    d_l_d_filters[k] += slice_martix.clone() * d_l_d_out[k].get(i, j);
+        for idx in 0..self.filter_num {
+            for i in 0..h - self.filter_size + 1 {
+                for j in 0..w - self.filter_size + 1 {
+                    d_l_d_filters[idx] +=
+                        self.last_input.slice(i, j, self.filter_size) * d_l_d_out[idx].get(i, j);
                 }
             }
         }
 
-        self.filters = (0..self.filter_num)
-            .map(|i| self.filters[i].clone() - d_l_d_filters[i].clone() * lr)
-            .collect::<Vec<_>>();
+        for i in 0..self.filter_num {
+            self.filters[i] -= d_l_d_filters[i].clone() * lr;
+        }
     }
+}
+
+fn convolve(input: &Matrix<f64>, filter: &Matrix<f64>) -> Matrix<f64> {
+    let (h1, w1) = input.shape();
+    let (h2, w2) = filter.shape();
+
+    let mut output = Matrix::new_zero(h1 - h2 + 1, w1 - w2 + 1);
+
+    let (h, w) = output.shape();
+
+    for i in 0..h {
+        for j in 0..w {
+            let sm = input.slice(i, j, w2);
+            let sum = sm.multiple_martix_sum(filter);
+            output.set(i, j, sum);
+        }
+    }
+    output
 }
 
 #[cfg(test)]
@@ -75,9 +83,9 @@ mod tests {
 
     #[test]
     fn test_conv_3x3() {
-        let mut conv = ConvLayer::new(3, 8);
+        let mut conv = ConvLayer::new(3, 1);
 
-        assert_eq!(conv.filter_num, 8);
+        assert_eq!(conv.filter_num, 1);
         assert_eq!(conv.filters[0].shape(), (3, 3));
 
         let input = Matrix::new(vec![
@@ -88,7 +96,26 @@ mod tests {
         ]);
         let output = conv.forward(&input);
 
-        assert_eq!(output.len(), 8);
+        assert_eq!(output.len(), 1);
         assert_eq!(output[0].shape(), (2, 2));
+    }
+
+    #[test]
+    fn test_conv_calc() {
+        let input = Matrix::new(vec![
+            vec![4.0, 3.0, 2.0, 1.0],
+            vec![5.0, 3.0, 6.0, 4.0],
+            vec![1.0, 2.0, 4.0, 3.0],
+        ]);
+
+        let filter = Matrix::new(vec![vec![1.0, 2.0], vec![2.0, 1.0]]);
+
+        let output = convolve(&input, &filter);
+
+        assert_eq!(output.shape(), (2, 3));
+        assert_eq!(
+            output.0,
+            vec![vec![23.0, 19.0, 20.0], vec![15.0, 23.0, 25.0]]
+        );
     }
 }
